1fd6641d21f40b748a985169d8d0e2691115a8f4
ERDEM TEKİN <erdemtekin@ERDEM-MacBook-Pro.local>
2026-03-05 23:02:19 +0300

    feat: tekli yemek yerine 3 cesitli menu onerisi eklendi

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ function donustur() {
     let sonuc = birim === "ml" ? (miktar / 15).toFixed(1) + " kaşık" : (miktar / 140).toFixed(1) + " bardak";
     document.getElementById('resultText').innerText = miktar ? sonuc : "Miktar girin!";
 }
+
 function yemekOner() {
-    const yemekler = ["Mercimek Çorbası", "Karnıyarık", "Mantı", "Taze Fasulye", "Hünkar Beğendi", "İskender", "Ali Nazik", "İçli Köfte"];
-    document.getElementById('menuResult').innerText = "Önerimiz: " + yemekler[Math.floor(Math.random() * yemekler.length)];
+    const corbalar = ["Mercimek Çorbası", "Ezogelin Çorbası", "Yayla Çorbası", "Domates Çorbası"];
+    const anaYemekler = ["Tas Kebabı", "Karnıyarık", "Taze Fasulye", "Hünkar Beğendi", "Orman Kebabı"];
+    const yanUrunler = ["Pirinç Pilavı", "Bulgur Pilavı", "Mevsim Salata", "Cacık"];
+
+    const secilenCorba = corbalar[Math.floor(Math.random() * corbalar.length)];
+    const secilenYemek = anaYemekler[Math.floor(Math.random() * anaYemekler.length)];
+    const secilenYan = yanUrunler[Math.floor(Math.random() * yanUrunler.length)];
+
+    document.getElementById('menuResult').innerHTML = 
+        "🥣 " + secilenCorba + "<br>" + 
+        "🥘 " + secilenYemek + "<br>" + 
+        "🍚 " + secilenYan;
 }
+
 function tatliOner() {
     const tatlilar = ["Sütlaç", "Baklava", "Künefe", "Magnolia", "Güllaç"];
     document.getElementById('dessertResult').innerText = "Tatlımız: " + tatlilar[Math.floor(Math.random() * tatlilar.length)];
