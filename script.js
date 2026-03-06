243edfb2bc098f6ab270219665636a56a6e35c0b
ERDEM TEKİN <erdemtekin@ERDEM-MacBook-Pro.local>
2026-03-06 21:54:01 +0300

    feat: yan urunlere humus eklendi

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function yemekOner() {
     ];
     const yanUrunler = [
         "Pirinç Pilavı", "Bulgur Pilavı", "Mevsim Salata", "Cacık", 
-        "Gavurdağı Salatası", "Şehriye Pilavı", "Haydari", "Çoban Salata"
+        "Gavurdağı Salatası", "Şehriye Pilavı", "Haydari", "Çoban Salata", "Humus"
     ];
 
     const secilenCorba = corbalar[Math.floor(Math.random() * corbalar.length)];
