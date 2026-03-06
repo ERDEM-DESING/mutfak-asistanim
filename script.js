f79dcd46ca10fc408a56f160c1afd5902f54b2d8
ERDEM TEKİN <erdemtekin@ERDEM-MacBook-Pro.local>
2026-03-06 22:06:00 +0300

    feat: yan urunlere mucver eklendi

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function yemekOner() {
     ];
     const yanUrunler = [
         "Pirinç Pilavı", "Bulgur Pilavı", "Mevsim Salata", "Cacık", 
-        "Gavurdağı Salatası", "Şehriye Pilavı", "Haydari", "Çoban Salata", "Humus", "Acılı Ezme", "Patates Kızartması", "Rus Salatası", "Şaksuka", "Deniz Börülcesi"
+        "Gavurdağı Salatası", "Şehriye Pilavı", "Haydari", "Çoban Salata", "Humus", "Acılı Ezme", "Patates Kızartması", "Rus Salatası", "Şaksuka", "Deniz Börülcesi", "Mücver"
     ];
 
     const secilenCorba = corbalar[Math.floor(Math.random() * corbalar.length)];
