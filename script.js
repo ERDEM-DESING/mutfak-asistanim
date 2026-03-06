fba09d4ef20bf590fd8af44e0c68e779a6861533
ERDEM TEKİN <erdemtekin@ERDEM-MacBook-Pro.local>
2026-03-06 21:55:31 +0300

    feat: yan urunlere patates kizartmasi eklendi

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function yemekOner() {
     ];
     const yanUrunler = [
         "Pirinç Pilavı", "Bulgur Pilavı", "Mevsim Salata", "Cacık", 
-        "Gavurdağı Salatası", "Şehriye Pilavı", "Haydari", "Çoban Salata", "Humus", "Acılı Ezme"
+        "Gavurdağı Salatası", "Şehriye Pilavı", "Haydari", "Çoban Salata", "Humus", "Acılı Ezme", "Patates Kızartması"
     ];
 
     const secilenCorba = corbalar[Math.floor(Math.random() * corbalar.length)];
