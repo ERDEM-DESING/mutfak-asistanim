fc5340841afc0e37d22d97c976375920dc919f8f
ERDEM TEKİN <erdemtekin@ERDEM-MacBook-Pro.local>
2026-03-06 21:50:57 +0300

    feat: tatli listesine revani eklendi

--- script.js
+++ script.js
@@ -31,6 +31,6 @@ function yemekOner() {
 }
 
 function tatliOner() {
-    const tatlilar = ["Sütlaç", "Baklava", "Künefe", "Magnolia", "Güllaç", "Kazandibi", "Şekerpare", "Fırın Sütlaç", "Kazandibi", "Kıbrıs Tatlısı", "Şekerpare", "Sufle", "Profiterol", "Kabak Tatlısı", "Ayva Tatlısı"];
+    const tatlilar = ["Sütlaç", "Baklava", "Künefe", "Magnolia", "Güllaç", "Kazandibi", "Şekerpare", "Fırın Sütlaç", "Kazandibi", "Kıbrıs Tatlısı", "Şekerpare", "Sufle", "Profiterol", "Kabak Tatlısı", "Ayva Tatlısı", "Revani"];
     document.getElementById('dessertResult').innerText = "Tatlımız: " + tatlilar[Math.floor(Math.random() * tatlilar.length)];
 }
