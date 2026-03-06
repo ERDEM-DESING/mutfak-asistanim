e835a06a59d774ddfceffbf1b6a5593eb8d5a375
ERDEM TEKİN <erdemtekin@ERDEM-MacBook-Pro.local>
2026-03-06 21:39:51 +0300

    feat: tatli listesine kazandibi eklendi

--- script.js
+++ script.js
@@ -31,6 +31,6 @@ function yemekOner() {
 }
 
 function tatliOner() {
-    const tatlilar = ["Sütlaç", "Baklava", "Künefe", "Magnolia", "Güllaç", "Kazandibi", "Şekerpare", "Fırın Sütlaç"];
+    const tatlilar = ["Sütlaç", "Baklava", "Künefe", "Magnolia", "Güllaç", "Kazandibi", "Şekerpare", "Fırın Sütlaç", "Kazandibi"];
     document.getElementById('dessertResult').innerText = "Tatlımız: " + tatlilar[Math.floor(Math.random() * tatlilar.length)];
 }
